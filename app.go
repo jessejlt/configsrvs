848da00ea8f03b400b57051badc0990f332b47ae
Jesse Thompson <jessejlt@adobe.com>
2014-06-08 16:43:03 -0700

    Add expvar for request count and DataStore ref

--- app.go
+++ app.go
@@ -1,29 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"github.com/codegangsta/negroni"
-	"log"
-	"net/http"
+  "expvar"
+  "fmt"
+  "github.com/codegangsta/negroni"
+  "log"
+  "net/http"
+)
+
+var (
+  RequestCount = expvar.NewInt("request_count")
+  Store        DataStore
 )
 
 func main() {
 
-	cli, err := Commands()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Options = %v\n", cli)
-	dataStore, err := GetDataStore(cli.DataStore)
-	if err != nil {
-		log.Fatal(err)
-	}
-	dataStore.validate()
+  cli, err := Commands()
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Printf("Options = %v\n", cli)
+  dataStore, err := GetDataStore(cli.DataStore)
+  if err != nil {
+    log.Fatal(err)
+  }
+  Store = dataStore
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", HomeHandler)
+  mux := http.NewServeMux()
+  mux.HandleFunc("/", HomeHandler)
 
-	n := negroni.Classic()
-	n.UseHandler(mux)
-	n.Run(fmt.Sprintf(":%d", cli.Port))
+  n := negroni.Classic()
+  n.UseHandler(mux)
+  n.Run(fmt.Sprintf(":%d", cli.Port))
 }
