bd2277ab02f84b222afa6861120812c1d6b7ec9a
Jesse Thompson <jessejlt@adobe.com>
2014-06-07 19:37:09 -0700

    Add S3 bucket CLI

--- commands.go
+++ commands.go
@@ -3,12 +3,14 @@ package main
 import "flag"
 
 type CLI struct {
-	Port int
+	Port   int
+	Bucket string
 }
 
 func Commands() (CLI, error) {
 	cli := CLI{}
-	flag.IntVar(&cli.Port, "port", 8080, "Add custom port number")
+	flag.IntVar(&cli.Port, "port", 8080, "Custom port number")
+	flag.StringVar(&cli.Bucket, "bucket", "config", "AWS S3 bucket name")
 	flag.Parse()
 
 	return cli, nil
