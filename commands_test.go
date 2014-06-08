d2b5b904d5e064bebb6a37c50bab8fe342b85adc
Jesse Thompson <jessejlt@adobe.com>
2014-06-08 16:42:05 -0700

    Modify arguments for gofmt
    Use soft tabs of size 2

--- commands_test.go
+++ commands_test.go
@@ -4,23 +4,23 @@ import "testing"
 
 func TestEmptyArgs(t *testing.T) {
 
-  const port = 8080
-  const bucket = "config"
-  const dataStore = "s3"
-  cli, err := Commands()
-  if err != nil {
-    t.Error(err)
-  }
+	const port = 8080
+	const bucket = "config"
+	const dataStore = "s3"
+	cli, err := Commands()
+	if err != nil {
+		t.Error(err)
+	}
 
-  if port != cli.Port {
-    t.Error("Expected %v, Received %v", port, cli.Port)
-  }
+	if port != cli.Port {
+		t.Error("Expected %v, Received %v", port, cli.Port)
+	}
 
-  if bucket != cli.Bucket {
-    t.Error("Expected %v, Received %v", bucket, cli.Bucket)
-  }
+	if bucket != cli.Bucket {
+		t.Error("Expected %v, Received %v", bucket, cli.Bucket)
+	}
 
-  if dataStore != cli.DataStore {
-    t.Error("Expected %v, Received %v", dataStore, cli.DataStore)
-  }
+	if dataStore != cli.DataStore {
+		t.Error("Expected %v, Received %v", dataStore, cli.DataStore)
+	}
 }
--- s3.go
+++ s3.go
@@ -1,32 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"log"
+  "io/ioutil"
+  "log"
 )
 
 type S3 struct {
-	Credentials Creds
-	ETag        string
+  Credentials Creds
+  ETag        string
 }
 
 func (s S3) validate() bool {
 
-	creds, err := AWSCredentials()
-	if err != nil {
-		log.Fatal(err)
-		return false
-	}
-	fmt.Printf("Using AWS Creds = %v\n", creds)
-	s.Credentials = creds
+  creds, err := AWSCredentials()
+  if err != nil {
+    log.Fatal(err)
+    return false
+  }
+  s.Credentials = creds
 
-	return true
+  return true
 }
 
 func (s S3) fetch() (string, error) {
-	return "", nil
+
+  data, err := ioutil.ReadFile("config.json")
+  if err != nil {
+    return "", err
+  }
+
+  return string(data), nil
 }
 
 func (s S3) update(data string) error {
-	return nil
+  return nil
 }
